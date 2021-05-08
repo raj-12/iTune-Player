279753849ad60a9728342cd82a49a2bbb9639416
raj-12 <rajtawari15@gmail.com>
2021-05-08 13:48:03 +0530

    updation

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,17 @@
 import React from "react";
+import './header.css'
 
 const Header = () => {
     return (
-      <div style={{height:"35px"}}>
 
-            <nav className="navbar navbar-expand-lg navbar-light">
-              <div className="container-fluid">
+            <nav className="navbar navbar-expand-lg navbar-light" id="navhe" >
+              <div className="container" >
                     <a className="navbar-brand" id="icon" href="/">iTunes</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                           <span className="navbar-toggler-icon"></span>
                     </button>
 
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup" >
                         <div className="navbar-nav">
                             <a className="nav-link active" aria-current="page" href="/">Home</a>
                             <a className="nav-link active" href="/about">About</a>
@@ -20,11 +20,6 @@ const Header = () => {
 
               </div>
             </nav>
-
-
-
-
-        </div>
     );
 };
 
